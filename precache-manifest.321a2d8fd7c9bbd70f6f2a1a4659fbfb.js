e5f18b6726687c292e5ab6dc0b5ca927d2507ebb
Rafi74 <70798894+Rafi74@users.noreply.github.com>
2020-09-05 23:35:12 +0600

    Updates

---
+++ precache-manifest.321a2d8fd7c9bbd70f6f2a1a4659fbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc211975fba1801db96c5322f9dd2f22",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "e57c9b3d98641b4b8d94",
+    "url": "/frontend/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "d5655a6064a411c1d000",
+    "url": "/frontend/static/js/2.38f4f618.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/frontend/static/js/2.38f4f618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57c9b3d98641b4b8d94",
+    "url": "/frontend/static/js/main.35975fc3.chunk.js"
+  },
+  {
+    "revision": "747c638a0ed8091beb0a",
+    "url": "/frontend/static/js/runtime-main.12e52466.js"
+  }
+]);
